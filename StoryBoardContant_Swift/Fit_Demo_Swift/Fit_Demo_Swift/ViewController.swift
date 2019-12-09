a0612491f81d2251eaf25a8d1bd209b59be22ca8
zhaofa-hp <zhaofa@huapubj.com>
2019-12-09 10:40:58 +0800

    feat:Swift 拖拽布局自适应

---
+++ StoryBoardContant_Swift/Fit_Demo_Swift/Fit_Demo_Swift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LabelExtension.swift
+//  Demo
+//
+//  Created by yangjunjie on 2019/7/18.
+//  Copyright © 2019年 yangjunjie. All rights reserved.
+//
+
+import UIKit
+
+private var ex_na_code = 1024
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
